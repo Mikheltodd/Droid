13fb918c71b11faf5cb6f2b592df18f00cec84db
Mikheltodd <68166999+Mikheltodd@users.noreply.github.com>
2025-03-04 19:18:24 -0500

    Initial commit

---
+++ src/Droid.java
@@ -0,0 +1,24 @@
+public class Droid {
+    // instance fields
+    int batteryLevel;
+    String name;
+    // constructor method
+    public Droid(String droidName) {
+        name = droidName;
+        batteryLevel = 100;
+    }
+
+    public String toString(){
+        return "Hello, my name is " + name;
+    }
+
+    public void energyReport(){
+        System.out.println("Current battery level: " + batteryLevel + "%.");
+    }
+
+    public void performTask(String task){
+        System.out.println(name + " is performing: " + task);
+        batteryLevel -= 10;
+        energyReport();
+    }
+}
